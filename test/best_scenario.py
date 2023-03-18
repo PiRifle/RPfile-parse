8147e732962fb5decb88956db4df29fb969ca48a
PiRifle <piotrekpolocz@gmail.com>
2023-03-18 20:27:19 +0100

    yes

--- test/best_scenario.py
+++ test/best_scenario.py
@@ -1,8 +1,9 @@
+from asyncio import wait
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 from rich.progress import Progress, BarColumn, DownloadColumn, TransferSpeedColumn, TimeRemainingColumn
-
+from os.path import abspath
 progress = Progress()
 
 def download(url):
@@ -10,21 +11,25 @@ def download(url):
     total_size = int(response.headers.get("content-length", 0))
     block_size = 524288
     progress_bar = progress.add_task(f"downloading {url}", total=total_size)
-    with progress:
-        with open(url.split("/")[-1], "wb") as file:
-            for data in response.iter_content(block_size):
-                progress.update(progress_bar, advance=len(data))
-                file.write(data)
-            progress.remove_task(progress_bar)
+    with open(url.split("/")[-1], "wb") as file:
+        for data in response.iter_content(block_size):
+            progress.update(progress_bar, advance=len(data))
+            file.write(data)
+        progress.remove_task(progress_bar)
+    return abspath(url.split("/")[-1])
 
 
-
+def parallel_download(links):
+    with ThreadPoolExecutor(4) as executor:
+        futures = [executor.submit(download, i) for i in links]
+        wait(futures)
+        for out in as_completed(futures):
+            yield out.result()
 
 urls = [
-    "http://192.168.8.182:3000/windows10_vm.img",
-    "http://192.168.8.182:3000/ubuntu_server_2022.img",
-    "http://192.168.8.182:3000/centos.iso",
-    "http://192.168.8.182:3000/zip_10MB.zip",
+    "http://localhost:3000/testfile100mb.bin",
+    "http://localhost:3000/testfile10mb.bin",
+    # "http://localhost:3000/testfile1gb.bin",
 ]
-for i in urls:
-    download(i)
+with progress:
+    print(list(parallel_download(urls)))
--- test/main.py
+++ test/main.py
@@ -1,11 +1,40 @@
 from rpfile_parse import RPFileParser
 from utils import parse_arguments
 import argparse
+from urllib import parse
 import rich
+from asyncio import wait
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+from rich.progress import Progress, BarColumn, DownloadColumn, TransferSpeedColumn, TimeRemainingColumn
+from os.path import abspath
+import os
+import uuid
+progress = Progress()
 
-rp_file = RPFileParser("./")
+def download(url):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 524288
+    progress_bar = progress.add_task(f"downloading {url}", total=total_size)
+    with open(url.split("/")[-1], "wb") as file:
+        for data in response.iter_content(block_size):
+            progress.update(progress_bar, advance=len(data))
+            file.write(data)
+        progress.remove_task(progress_bar)
+    return abspath(url.split("/")[-1])
 
-OPTIONS = {"server": rp_file.args.get("SERVER", "http://server.pingus/")}
+def download_command(command):
+    return {"path": download(command["url"]), **command}
+
+def parallel_download_commands(links):
+    with progress:
+        with ThreadPoolExecutor(4) as executor:
+            futures = [executor.submit(download_command, i) for i in links]
+            for out in as_completed(futures):
+                yield out.result()
+
 
 def _deploy(_from, _to):
     pass
@@ -28,8 +57,29 @@ INSTRUCTIONS = {
     "UNPACK": _unpack
 }
 
-for step in rp_file.structure:
-    INSTRUCTIONS.get(step["instruction"], _ignore)(*parse_arguments(step["value"]))
+def _parse_download_commands(rp: RPFileParser):
+    config_url = parse.urlparse(os.environ["server"]) 
+    for step in rp.structure:
+        if step["instruction"] in ["COPY", "UNPACK"]:
+            url: parse.ParseResult = parse.urlparse(parse_arguments(step["value"])[0])
+            if not url.scheme:
+                url = url._replace(scheme=config_url.scheme)
+            if not url.netloc:
+                url = url._replace(netloc=config_url.netloc)
+            # if url.scheme in ["http", "https"]:
+            step["url"] = url.geturl()
+            yield step
+
+rp_file = RPFileParser("./")
+
+OPTIONS = {"server": rp_file.args.get("SERVER", "http://server.pingus/")}
+for option in OPTIONS:
+    os.environ[option] = OPTIONS[option]
+
+for command in parallel_download_commands(_parse_download_commands(rp_file)):
+    INSTRUCTIONS.get(command["instruction"], _ignore)(command["path"],parse_arguments(command["value"])[-1])
+
+# for step in rp_file.structure:
 
     # print(parser.parse_args())
 
